b217693d06d1dc0471e3bd9f96f86d9f1848be58
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-05 18:32:39 +0000

    added productivity view

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,165 +1,231 @@
-import React, { Component } from 'react';
-import './App.css';
-import Modal from './components/Modal';
-import axios from 'axios';
+import React, { Component } from 'react'
+import './App.css'
+import Modal from './components/Modal'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       modal: false,
       viewCompleted: false,
       activeItem: {
-        title: "",
-        description: "",
-        completed: false
+        title: '',
+        description: '',
+        completed: false,
       },
-      todoList: []
-    };
+      todoList: [],
+      showProductivity: false,
+    }
   }
 
   componentDidMount() {
-    this.refreshList();
+    this.refreshList()
   }
 
   refreshList = () => {
     axios
-      .get("http://localhost:8000/api/tasks/")
-      .then(res => {
-        const updatedTasks = res.data.map(task => {
-          const isOverdue = new Date(task.due_date) < new Date();
+      .get('http://localhost:8000/api/tasks/')
+      .then((res) => {
+        const updatedTasks = res.data.map((task) => {
+          const isOverdue = new Date(task.due_date) < new Date()
           return {
             ...task,
-            isOverdue
-          };
-        });
-        this.setState({ todoList: updatedTasks });
+            isOverdue,
+          }
+        })
+        this.setState({
+          todoList: updatedTasks,
+        })
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err))
   }
 
   toggle = () => {
-    this.setState({ modal: !this.state.modal });
+    this.setState({
+      modal: !this.state.modal,
+    })
   }
 
-  handleSubmit = item => {
-    this.toggle();
+  handleSubmit = (item) => {
+    this.toggle()
     if (item.id) {
       axios
         .put(`http://localhost:8000/api/task/${item.id}/`, item)
-        .then(res => this.refreshList())
-        .catch(err => console.error("Update Error:", err));
+        .then((res) => this.refreshList())
+        .catch((err) => console.error('Update Error:', err))
     } else {
       axios
-        .post("http://localhost:8000/api/task/", item)
-        .then(res => this.refreshList())
-        .catch(err => console.error("Create Error:", err));
+        .post('http://localhost:8000/api/task/', item)
+        .then((res) => this.refreshList())
+        .catch((err) => console.error('Create Error:', err))
     }
   }
 
-  handleDelete = item => {
+  handleDelete = (item) => {
     axios
       .delete(`http://localhost:8000/api/task/${item.id}/`)
-      .then(res => this.refreshList())
-      .catch(err => console.error("Delete Error:", err));
+      .then((res) => this.refreshList())
+      .catch((err) => console.error('Delete Error:', err))
   }
 
   createItem = () => {
-    const item = { title: "", description: "", completed: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  }
-
-  editItem = item => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  }
-
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    } else {
-      return this.setState({ viewCompleted: false });
+    const item = {
+      title: '',
+      description: '',
+      completed: false,
     }
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal,
+    })
+  }
+
+  editItem = (item) => {
+    this.setState({
+      activeItem: item,
+      modal: !this.state.modal,
+    })
+  }
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({
+        viewCompleted: true,
+      })
+    } else {
+      return this.setState({
+        viewCompleted: false,
+      })
+    }
+  }
+
+  handleComplete = (item) => {
+    // Toggle completion status
+    const updatedItem = {
+      ...item,
+      completed: !item.completed,
+    }
+
+    axios
+      .put(`http://localhost:8000/api/task/${item.id}/`, updatedItem)
+      .then((res) => this.refreshList())
+      .catch((err) => console.error('Completion Error:', err))
+  }
+
+  calculateProductivity = () => {
+    const completedTasks = this.state.todoList.filter(
+      (task) => task.completed
+    ).length
+    const totalTasks = this.state.todoList.length
+    return totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0
   }
 
   renderTabList = () => {
     return (
-      <div className='my-5 tab-list'>
+      <div className="my-5 tab-list">
         <span
           onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "active" : ""}
+          className={this.state.viewCompleted ? 'active' : ''}
         >
-          Completed
+          Completed{' '}
         </span>
-
         <span
           onClick={() => this.displayCompleted(false)}
-          className={!this.state.viewCompleted ? "active" : ""}
+          className={!this.state.viewCompleted ? 'active' : ''}
         >
-          Incomplete
-        </span>
+          Incomplete{' '}
+        </span>{' '}
       </div>
-    );
+    )
   }
 
   renderItems = () => {
-    const { viewCompleted } = this.state;
+    const { viewCompleted } = this.state
     const newItems = this.state.todoList.filter(
-      item => item.completed === viewCompleted
-    );
+      (item) => item.completed === viewCompleted
+    )
 
-    return newItems.map(item => (
+    return newItems.map((item) => (
       <li
-        className='list-group-item d-flex justify-content-between align-items-center'
+        className="list-group-item d-flex justify-content-between align-items-center"
         key={item.id}
       >
         <span
-          className={`mytask-title mr-2 ${this.state.viewCompleted ? "completed-task" : ""}`}
+          className={`mytask-title mr-2 ${
+            this.state.viewCompleted ? 'completed-task' : ''
+          }`}
           title={item.title}
         >
-          {item.title}
-        </span>
-        <span className='due-date mr-2'>
-          Due: {item.due_date}
-        </span>
+          {' '}
+          {item.title}{' '}
+        </span>{' '}
+        <span className="due-date mr-2">Due: {item.due_date} </span>{' '}
         <button
-          className='btn btn-info mr-2'
+          className="btn btn-info mr-2"
           onClick={() => this.editItem(item)}
         >
-          Edit
-        </button>
+          Edit{' '}
+        </button>{' '}
         <button
-          className='btn btn-danger'
+          className="btn btn-danger"
           onClick={() => this.handleDelete(item)}
         >
-          Delete
-        </button>
+          Delete{' '}
+        </button>{' '}
       </li>
-    ));
+    ))
   }
 
   render() {
     return (
-      <main className='content p-3 mb-2 bg-info'>
-        <h1 className='text-black text-uppercase text-center my-4'>Task Manager</h1>
-        <div className='row'>
-          <div className='col-md-6 col-sma-10 mx-auto p-0'>
-            <div className='card p-3'>
+      <main className="content p-3 mb-2 bg-info">
+        <h1 className="text-black text-uppercase text-center my-4">
+          Task Manager
+        </h1>
+        <div className="row">
+          <div className="col-md-6 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
               <div>
-                <button className='btn btn-primary' onClick={this.createItem}>
+                <button
+                  className="btn btn-primary mb-4"
+                  onClick={this.createItem}
+                >
                   Add Task
                 </button>
               </div>
+
+              {/* Button to toggle productivity display */}
+              <div>
+                <button
+                  className="btn btn-secondary"
+                  onClick={this.toggleProductivity}
+                >
+                  {this.state.showProductivity
+                    ? 'Hide Productivity'
+                    : 'Show Productivity'}
+                </button>
+              </div>
+
+              {/* Conditional rendering of productivity display */}
+              {this.state.showProductivity && (
+                <div className="productivity-display">
+                  <h2>
+                    Productivity: {this.calculateProductivity().toFixed(2)}%
+                  </h2>
+                </div>
+              )}
+
               {this.renderTabList()}
-              <ul className='list-group list-group-flush'>
+              <ul className="list-group list-group-flush border-success">
                 {this.renderItems()}
               </ul>
             </div>
           </div>
         </div>
-        <footer className='my-5 mb-2 bg-info text-center'>
-          Copyright 2023 © All rights Reserved
+        <footer className="my-5 mb-2 bg-info text-center">
+          Copyright 2023© All rights Reserved
         </footer>
-
         {this.state.modal ? (
           <Modal
             activeItem={this.state.activeItem}
@@ -168,8 +234,8 @@ class App extends Component {
           />
         ) : null}
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- fronted/src/App.test.js
+++ fronted/src/App.test.js
@@ -1,8 +1,70 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen
+} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render( < App / > );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+import React from 'react';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+import axios from 'axios';
+
+// Mock axios for all HTTP requests
+jest.mock('axios');
+
+describe('App component', () => {
+  it('renders add task input field', () => {
+    render( < App / > );
+    expect(screen.getByPlaceholderText('Enter task title')).toBeInTheDocument();
+  });
+
+  it('allows users to input a task', () => {
+    render( < App / > );
+    const input = screen.getByPlaceholderText('Enter task title');
+    fireEvent.change(input, {
+      target: {
+        value: 'New Task'
+      }
+    });
+    expect(input.value).toBe('New Task');
+  });
+
+  it('adds a new task when add button is clicked', async () => {
+    // Mock a successful response
+    axios.post.mockResolvedValueOnce({
+      data: {
+        id: 1,
+        title: 'New Task',
+        completed: false
+      },
+    });
+
+    render( < App / > );
+    const input = screen.getByPlaceholderText('Enter task title');
+    fireEvent.change(input, {
+      target: {
+        value: 'New Task'
+      }
+    });
+    fireEvent.click(screen.getByText('Add Task'));
+
+    // Wait for the post call to complete and the component to re-render
+    await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
+    // Check if the input has been cleared after task is added
+    expect(input.value).toBe('');
+  });
+
+  // More tests can be added to cover other functionalities like marking tasks as complete, deleting tasks, etc.
 });
